e347694d0e8cfa5ebfa67448656667dbf32aa487
mahabube <info.freelancermahabub@gmail.com>
2023-11-22 21:01:10 +0600

    server setup successfully done

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+// application Routes
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello Server!');
+};
+app.get('/', getAController);
+
+export default app;
